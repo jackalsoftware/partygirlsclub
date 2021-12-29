ddf6384adda47174dda53c842e95e5b3cc774956
jackalsoftware <jackalsoftware@users.noreply.github.com>
2021-12-30 02:10:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ export const StyledLogo = styled.img`
 export const StyledImg = styled.img`
   box-shadow: 0px 5px 11px 2px rgba(0, 0, 0, 0.7);
   border: 4px dashed var(--secondary);
-  background-color: var(--accent);
+  background-color: #d3e7eb00;
   border-radius: 100%;
   width: 200px;
   @media (min-width: 900px) {
