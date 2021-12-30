9d3c1ea01f9be00e62731dec9269ae10c8dbce54
jackalsoftware <jackalsoftware@users.noreply.github.com>
2021-12-30 19:16:07 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -241,7 +241,7 @@ function App() {
               </StyledLink>
             </s.TextDescription>
             <s.SpacerSmall />
-            {Number(data.totalSupply) >= CONFIG.MAX_SUPPLY ? (
+             {Number(data.totalSupply) >= CONFIG.MAX_SUPPLY ? (
               <>
                 <s.TextTitle
                   style={{ textAlign: "center", color: "var(--accent-text)" }}
